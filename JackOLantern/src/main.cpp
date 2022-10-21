d12c25934bc24bb24cfe9e1e4684013a6bc33cd9
William Redenbaugh <wmredenbaugh@gmail.com>
2022-10-21 10:56:45 -0700

    fixed fourier transforms

--- JackOLantern/src/main.cpp
+++ JackOLantern/src/main.cpp
@@ -6,15 +6,15 @@
 #include "arduinoFFT.h"
 
 /////////////////// THREAD INIT BEGIN ///////////////////
-#define FFT_COMPUTE_STACK_SIZE 1024
+#define FFT_COMPUTE_STACK_SIZE 1724
 uint8_t fft_compute_stack[FFT_COMPUTE_STACK_SIZE];
 os_thread_id_t fft_compute_thread_handler;
 
-#define ANALOG_READ_STACK_SIZE 64
+#define ANALOG_READ_STACK_SIZE 32
 uint8_t analog_read_stack[ANALOG_READ_STACK_SIZE];
 os_thread_id_t analog_read_thread_handler;
 
-#define MATRIX_DISPLAY_STACK_SIZE 128
+#define MATRIX_DISPLAY_STACK_SIZE 512
 uint8_t matrix_display_stack[MATRIX_DISPLAY_STACK_SIZE];
 os_thread_id_t matrix_display_thread_handler;
 /////////////////// THREAD INIT END ///////////////////
@@ -27,7 +27,7 @@ os_thread_id_t matrix_display_thread_handler;
 #define SCL_FREQUENCY 0x02
 #define SCL_PLOT 0x03
 
-#define NUM_SAMPLES 256
+#define NUM_SAMPLES 128
 // Buffer for getting microphone samples
 MutexLock mic_muttx;
 uint8_t mic_input[NUM_SAMPLES];
@@ -48,7 +48,7 @@ uint8_t data_output_scaled[NUM_SAMPLES];
 /////////////////// MATRIX VARS END ///////////////////
 
 static void analog_read_task(void *arg){
-  analogReadResolution(8);
+  analogReadResolution(2);
   for(;;){
     // Safely gather up samples to be used for fourier transforms
     mic_muttx.lockWaitIndefinite();
@@ -57,7 +57,7 @@ static void analog_read_task(void *arg){
     }
     mic_muttx.unlock();
 
-    os_thread_delay_ms(30);
+    os_thread_delay_ms(10);
   }
 }
 
@@ -77,19 +77,23 @@ static void fft_compute_task(void *arg){
     memset(vImag, 0, sizeof(vImag));
 
     fft.Windowing(vReal, NUM_SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
-    fft.Compute(vReal, vImag, exponent, FFT_FORWARD);
+    fft.Compute(vReal, vImag, NUM_SAMPLES, FFT_FORWARD);
     fft.ComplexToMagnitude(vReal, vImag, NUM_SAMPLES);
 
     // Copy data over to be used by LED matrix
     data_output_muttx.lockWaitIndefinite();
+    
+
     for(int n = 0; n < NUM_SAMPLES; n++){
-      data_output[n] = int(vImag[n]);
+      data_output[n] = int(vReal[n]);
     }
     data_output_muttx.unlock();
 
-    os_thread_delay_ms(30);
+    os_thread_delay_ms(10);
   }
 }
+
+uint8_t matrix_length[NUMBER_OF_DEVICES * 8];
 
 static void matrix_display_task(void *arg){
   UNUSED(arg);
@@ -98,21 +102,36 @@ static void matrix_display_task(void *arg){
   ledMatrix.init();
   ledMatrix.clear();
   ledMatrix.setIntensity(5);
+  memset(matrix_length, 0, sizeof(matrix_length));
+  const int multiplier = NUM_SAMPLES / (NUMBER_OF_DEVICES * 8);
 
   for(;;){
     // Copy and scale the data over from the FFT thread
     data_output_muttx.lockWaitIndefinite();
     for(int n = 0; n < NUM_SAMPLES; n++){
-      data_output_scaled[n] = data_output[n]/16;
+      data_output_scaled[n] = data_output[n];
     }
     data_output_muttx.unlock();
+
+    for(int x = 0; x < (NUMBER_OF_DEVICES * 8); x++){
+      int y_len = data_output_scaled[multiplier * x + 2];
+
+      if(y_len > 32)
+        y_len = 32; 
+
+      if(y_len > matrix_length[x])
+        matrix_length[x] = y_len;
+
+      if(matrix_length[x] > 0){
+        matrix_length[x]--;
+      }
+    }
 
     // Clear all pixels so we start with an empty tab
     ledMatrix.clear();
     // Compute and setup display for showing the volume level
     for(int x = 0; x < (NUMBER_OF_DEVICES * 8); x++){
-      const int multiplier = NUM_SAMPLES / (NUMBER_OF_DEVICES * 8);
-      int y_len = data_output_scaled[multiplier * x];
+      int y_len = matrix_length[x];
       for(int y = 0; y < y_len; y++){
         ledMatrix.setPixel(x, y);
       }
@@ -120,7 +139,7 @@ static void matrix_display_task(void *arg){
 
     // Push out data to the display
     ledMatrix.commit();
-    os_thread_delay_ms(30);
+    os_thread_delay_ms(10);
   }
 }
 
@@ -135,6 +154,7 @@ void setup() {
   SystemClock_Config();
 
   os_init();
+  init_thread();
 }
 
 void loop() {
